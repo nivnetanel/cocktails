01f951a612f800f1a1a990707a05de9c1dd5ab90
niv netanel <nivnetanel@gmail.com>
2022-04-22 19:20:14 +0300

    first commit

---
+++ src/components/Cocktail.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Cocktail({ image, name, id, info, glass }) {
+  return (
+    <Link to={`/cocktail/${id}`}>
+      <article className="cocktail">
+        <div className="img-container">
+          <img src={image} alt={name} />
+        </div>
+        <div className="cocktail-footer">
+          <h3>{name}</h3>
+          <h4>{glass}</h4>
+          <p>{info}</p>
+        </div>
+      </article>
+    </Link>
+  );
+}
